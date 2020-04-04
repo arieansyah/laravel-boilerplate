c880d5cbf936b024b83232956c4df96ebb2e6e77
arieansyah <arieansyahp.bun@gmail.com>
2020-04-04 10:01:59 +0700

    make crud hotels

---
+++ app/Http/Controllers/Backend/API/HotelController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers\Backend\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Hotel\Hotel;
+use App\Http\Requests\HotelRequest;
+use App\Http\Resources\Hotel as HotelResourceCollection;
+
+
+class HotelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json('siap', 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(HotelRequest $request, Hotel $hotel)
+    {
+        $store_hotel = Hotel::create($request->all());
+        return new HotelResourceCollection($store_hotel);
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $hotel = Hotel::find($id);
+        return new HotelResourceCollection($hotel);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(HotelRequest $request, $id)
+    {
+        Hotel::find($id)->update($request->all());
+        return new HotelResourceCollection(Hotel::find($id));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- app/Models/Hotel/Hotel.php
+++ app/Models/Hotel/Hotel.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Hotel extends Model
 {
-    protected $table = 'hotels';
-    protected $primaryKey = 'id';
+    // protected $table = 'hotels';
+    // protected $primaryKey = 'id';
+    protected $guarded = [];
+
 }
